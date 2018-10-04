2beaa9050d6fdc5784eb275563025897ea21495e
artbil <arthur.billion@student.uclouvain.be>
2018-10-04 15:28:41 +0200

    elon musk weed

--- mxgraph-master/javascript/src/js/eleccomponent.js
+++ mxgraph-master/javascript/src/js/eleccomponent.js
@@ -9,8 +9,6 @@ function VoltageShape() {};
 			{
 				if(w <= h)
 				{
-
-					
 					path.moveTo(0, h/2);
 					path.curveTo(0 , h/2 + 0.55*w/2, w/2 - 0.55*w/2, h/2 + w/2  ,w/2, h/2 + w/2);
 					path.curveTo(w/2 + 0.55*w/2, h/2 + w/2, w , h/2 + 0.55*w/2, w, h/2);
@@ -39,23 +37,19 @@ function VoltageShape() {};
 					path.lineTo(w/2, 3*h/8);
 
 					path.moveTo(w/2 - h/8, 3*h/4 );
-					path.lineTo(w/2 + h/8, 3*h/4);
-					
-					
+					path.lineTo(w/2 + h/8, 3*h/4);	
 				}
-
-
 			}
 		};
 		mxCellRenderer.registerShape('voltage', VoltageShape);
 
-function InductorShape() { };
-		InductorShape.prototype = new mxCylinder();
-		InductorShape.prototype.constructor = InductorShape;
-		InductorShape.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+function InductorShapeH() { };
+		InductorShapeH.prototype = new mxCylinder();
+		InductorShapeH.prototype.constructor = InductorShapeH;
+		InductorShapeH.prototype.redrawPath = function(path, x, y, w, h, isForeground)
 		{
 			var dx = w / 6;
-                        var dy = h / 6;
+            var dy = h / 6;
 			if (isForeground)
 			{
 				path.moveTo(0, h/2);
@@ -70,12 +64,35 @@ function InductorShape() { };
 				path.end();
 			}
 		};
-		mxCellRenderer.registerShape('inductor', InductorShape);
+		mxCellRenderer.registerShape('inductor horizontal', InductorShapeH);
 
-function CapacitorShape() { };
-		CapacitorShape.prototype = new mxCylinder();
-		CapacitorShape.prototype.constructor = CapacitorShape;
-		CapacitorShape.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+function InductorShapeV() { };
+		InductorShapeV.prototype = new mxCylinder();
+		InductorShapeV.prototype.constructor = InductorShapeV;
+		InductorShapeV.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+		{
+			var dx = w / 6;
+            var dy = h / 6;
+			if (isForeground)
+			{
+				path.moveTo(w/2, 0);
+				path.lineTo(w/2, dy);
+				path.lineTo(w/2-dx ,dy);
+				path.curveTo(w/2-dx ,dy, w/2-5*dx, 1.5*dy, w/2-dx, 2*dy);
+                path.curveTo(w/2-dx, 2*dy, w/2-5*dx, 2.5*dy, w/2-dx, 3*dy);
+				path.curveTo(w/2-dx, 3*dy, w/2-5*dx, 3.5*dy, w/2-dx, 4*dy);
+				path.curveTo(w/2-dx, 4*dy, w/2-5*dx, 4.5*dy, w/2-dx, 5*dy);
+				path.lineTo(w/2, 5*dy);
+				path.lineTo(w/2, h);
+				path.end();
+			}
+		};
+		mxCellRenderer.registerShape('inductor vertical', InductorShapeV);
+
+function CapacitorShapeH() { };
+		CapacitorShapeH.prototype = new mxCylinder();
+		CapacitorShapeH.prototype.constructor = CapacitorShapeH;
+		CapacitorShapeH.prototype.redrawPath = function(path, x, y, w, h, isForeground)
 		{
 			var dx = w / 16;
 			if (isForeground)
@@ -91,16 +108,39 @@ function CapacitorShape() { };
 				path.end();
 			}
 		};
-		mxCellRenderer.registerShape('capacitor', CapacitorShape);
+		mxCellRenderer.registerShape('capacitor horizontal', CapacitorShapeH);
 
-function ResistorShape() { };
-		ResistorShape.prototype = new mxCylinder();
-		ResistorShape.prototype.constructor = ResistorShape;
-		ResistorShape.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+function CapacitorShapeV() { };
+		CapacitorShapeV.prototype = new mxCylinder();
+		CapacitorShapeV.prototype.constructor = CapacitorShapeV;
+		CapacitorShapeV.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+		{
+			var dy = h / 16;
+			if (isForeground)
+			{
+				path.moveTo(w/2, 0);
+				path.lineTo(w/2, h/2-dy);
+				path.moveTo(0, h/2+dy);
+				path.lineTo(w, h/2+dy);
+				path.moveTo(0, h/2-dy);
+				path.lineTo(w, h/2-dy);
+				path.moveTo(w/2, h/2+dy);
+				path.lineTo(w/2, h);
+				path.end();
+			}
+		};
+		mxCellRenderer.registerShape('capacitor vertical', CapacitorShapeV);
+
+function ResistorShapeH() { };      
+        
+		ResistorShapeH.prototype = new mxCylinder();
+		ResistorShapeH.prototype.constructor = ResistorShapeH;
+		ResistorShapeH.prototype.redrawPath = function(path, x, y, w, h, isForeground)
 		{
 			var dx = w / 16;
 			if (isForeground)
 			{
+                path.fillAndStroke();
 				path.moveTo(0, h / 2);
 				path.lineTo(2 * dx, h / 2);
 				path.lineTo(3 * dx, 0);
@@ -112,6 +152,31 @@ function ResistorShape() { };
 				path.lineTo(14 * dx, h / 2);
 				path.lineTo(16 * dx, h / 2);
 				path.end();
+                path.stroke();
 			}
 		};
-		mxCellRenderer.registerShape('resistor', ResistorShape);
+		mxCellRenderer.registerShape('resistor horizontal', ResistorShapeH);
+
+function ResistorShapeV() { };      
+        
+		ResistorShapeV.prototype = new mxCylinder();
+		ResistorShapeV.prototype.constructor = ResistorShapeV;
+		ResistorShapeV.prototype.redrawPath = function(path, x, y, w, h, isForeground)
+		{
+			var dy = h / 16;
+			if (isForeground)
+			{
+				path.moveTo(w/2, 0);
+				path.lineTo(w/2, 2 * dy);
+				path.lineTo(0, 3 * dy);
+				path.lineTo(w, 5 * dy);
+				path.lineTo(0 ,7 * dy);
+				path.lineTo(w, 9 * dy);
+				path.lineTo(0 ,11 * dy);
+				path.lineTo(w ,13 * dy);
+				path.lineTo(w/2, 14 * dy);
+				path.lineTo(w/2, 16 * dy);
+				path.end();
+			}
+		};
+		mxCellRenderer.registerShape('resistor vertical', ResistorShapeV);
