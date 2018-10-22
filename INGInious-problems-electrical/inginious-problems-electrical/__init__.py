edb701f4faeb37653568e2c0e988ecff348dd59b
artbil <arthur.billion@student.uclouvain.be>
2018-10-22 17:47:49 +0200

    dammmm les gens

--- INGInious-problems-electrical/inginious-problems-electrical/__init__.py
+++ INGInious-problems-electrical/inginious-problems-electrical/__init__.py
@@ -99,7 +99,4 @@ def init(plugin_manager, course_factory, client, plugin_config):
     # TODO: Replace by shared static middleware and let webserver serve the files
 	plugin_manager.add_page('/plugins/electrical/static/(.+)', StaticMockPage)
 	plugin_manager.add_hook("javascript_header", lambda: "/plugins/electrical/static/mxgraph/mxClient.min.js")
-	plugin_manager.add_hook("javascript_header", lambda: "/plugins/electrical/static/mxgraph/jquery.min.js")
-	plugin_manager.add_hook("javascript_header", lambda: "/plugins/electrical/static/mxgraph/bootstrap.min.js")
-	#plugin_manager.add_hook("css", lambda: "/plugins/electrical/static/mxgraph/bootstrap.min.css")
 	course_factory.get_task_factory().add_problem_type(DisplayableElectricalProblem)
