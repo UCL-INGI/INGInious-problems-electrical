1abdfbeeaddeb052c77d4a0e77b3324c6e53dd6b
artbil <arthur.billion@student.uclouvain.be>
2018-10-23 14:24:26 +0200

    commit

--- Inginious/tutorial/electrical/parser_ngspice.py
+++ Inginious/tutorial/electrical/parser_ngspice.py
@@ -8,15 +8,13 @@ def main():
 	for i in range(len(tokens)) :
 		word = tokens[i]
 		letters = list(word) 
-		if(len(letters) > 2 and (letters[0] == 'v' or letters[0] == 'i') and letters[1] == '(') :
-			result += " " + word + ' = ' + tokens[i+2]
-		elif(letters[0] == '@') :
-			result += " " + word + ' = ' + tokens[i+2]
+		if(len(letters) > 2 and letters[0] == 'i' and letters[1] == '(') :
+			result = tokens[i+2]
+			if(abs(float(result)) == 1):
+				print("True")
+			else:
+				print("False")
 		my_file.close()
-	if(result == " v(w0) = 1.000000e+01 @r1[i] = 2.000000e+00 i(v) = -2.00000e+00") :
-		print("True")
-	else :
-		print("False")
 
 if __name__ == "__main__":
     main()
